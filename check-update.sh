a4c1701dff278ede1bdc7f513475026dcecb7f09
Bernhard Rosenkränzer <bero@lindev.ch>
2023-10-07 22:30:27 +0200

    3.27.7, adapt version check to updated website

--- check-update.sh
+++ check-update.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-curl https://cmake.org/download/ 2>/dev/null |grep 'Latest Release' |sed -e 's,.*(,,;s,).*,,' |sort -V |tail -n1
+curl https://cmake.org/download/ 2>/dev/null |grep 'Latest Release' |head -n1 |sed -e 's,.*(,,;s,).*,,' |sort -V |tail -n1
